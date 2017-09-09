aef168c141c5c9ba44f3ff97d47762889c2d1f22
karthikr8547 <31785887+karthikr8547@users.noreply.github.com>
2017-09-09 09:03:40 +0530

    Included express module

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-var http = require('http');
-
-var server = http.createServer(function(request, response) {
-
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello Azure..!");
-
+var express=require('node_modules/express');
+var app=express();
+app.get('/',function(req,res)
+{
+res.send('Hello World!');
 });
-
-var port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+var server=app.listen(3000,function() {});
