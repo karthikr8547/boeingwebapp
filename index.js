f69c32b8c104ba9b1df0f027f5d11012d9230df4
karthikr8547 <31785887+karthikr8547@users.noreply.github.com>
2017-09-09 13:08:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var http = require('http');
 
-var server = http.createServer(function(request, response) {
-
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
-
+var express=require('express');
+var app=express();
+app.get('/app',function(req,res)
+{
+    res.send('Hello World! -- App');
 });
 
 var port = process.env.PORT || 1337;
-server.listen(port);
+var server=app.listen(port,function() {});
 
 console.log("Server running at http://localhost:%d", port);
